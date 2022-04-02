bd81985facbaa32ba525c38e5a4ce2fc093eda98
Steve Alex <salex@mac.com>
2022-04-02 12:21:34 -0500

    comments dependent destroy

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :group
   belongs_to :player
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   def can_edit_destroy?
     Current.player.present? && self.player_id == Current.player.id
