25bca56dbf0980c954576c277111a4970e1edc73
Steve Alex <salex@mac.com>
2022-07-12 08:31:14 -0500

    score input allowed negative on non-number

--- app/javascript/controllers/scoreMate_controller.js
+++ app/javascript/controllers/scoreMate_controller.js
@@ -33,24 +33,34 @@ export default class extends Controller {
   // Val getters return either a valid numb or a blank '' (or zero if no quota which should not happen)
   get frontVal(){
     const toNum =  Number(this.mateFrontTarget.value)
-    if (isNaN(toNum) || toNum == 0 ) {
+    if (isNaN(toNum) || toNum == 0 || toNum < 0) {
       if (this.mateFrontTarget.nodeName == 'INPUT'){
         this.mateFrontTarget.value = ''
+        this.mateFrontTarget.classList.add('bg-red-100')
+        this.mateFrontTarget.classList.remove('bg-green-100')
+        this.mateBackTarget.select()
       }
       return('')
     }else{
+      this.mateFrontTarget.classList.remove('bg-red-100')
+      this.mateFrontTarget.classList.add('bg-green-100')
       return(toNum)
     }
   }
 
   get backVal(){
     const toNum =  Number(this.mateBackTarget.value)
-    if (isNaN(toNum) || toNum == 0 ) {
+    if (isNaN(toNum) || toNum == 0 || toNum < 0) {
       if (this.mateBackTarget.nodeName == 'INPUT'){
         this.mateBackTarget.value = ''
+        this.mateBackTarget.classList.add('bg-red-100')
+        this.mateBackTarget.classList.remove('bg-green-100')
+        this.mateBackTarget.select()
       }
       return('')
     }else{
+      this.mateBackTarget.classList.remove('bg-red-100')
+      this.mateBackTarget.classList.add('bg-green-100')
       return(toNum)
     }
   }
