78d77520531f31cc0a11c58d0cf2bc47f1be163f
Steve Alex <salex@mac.com>
2022-10-11 08:52:20 -0500

    most of w3.css replaced

--- app/helpers/clubs_helper.rb
+++ app/helpers/clubs_helper.rb
@@ -12,7 +12,7 @@ module ClubsHelper
   end
 
   def callout_warning(content)
-    tag.div class:'w3-container w3-pale-yellow w3-leftbar w3-border-yellow w3-display-container' do 
+    tag.div class:'px-4 mt-4 bg-yellow-100 border-l-8 border-yellow-500 relative' do 
       concat(tag.button('&cross;'.html_safe,
         class:"absolute -top-3 -right-3 bg-red-500 hover:bg-red-600 text-2xl w-10 h-10 rounded-full focus:outline-none text-white",
           onclick:"this.parentElement.style.display='none'")) 
@@ -30,7 +30,7 @@ module ClubsHelper
     end
   end
   def callout_info(content)
-    tag.div class:' bg-blue-200 mt-4 border-l-8 border-blue-500 relative' do  
+    tag.div class:'px-4 mt-4 bg-blue-200 border-l-8 border-blue-500 relative' do  
       concat(tag.button('&cross;'.html_safe,
         class:"absolute -top-3 -right-3 bg-red-500 hover:bg-red-600 text-2xl w-10 h-10 rounded-full focus:outline-none text-white",
           onclick:"this.parentElement.style.display='none'")) 
@@ -39,7 +39,7 @@ module ClubsHelper
     end
   end
   def callout_success(content)
-    tag.div class:'w3-container w3-pale-green w3-leftbar w3-border-green w3-display-container' do  
+    tag.div class:'px-4 mt-4 bg-green-100 border-l-8  border-green-500 relative' do  
       concat(tag.button('&cross;'.html_safe,
         class:"absolute -top-3 -right-3 bg-red-500 hover:bg-red-600 text-2xl w-10 h-10 rounded-full focus:outline-none text-white",
           onclick:"this.parentElement.style.display='none'")) 
--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,17 @@
 module CommentsHelper
+
+  def css_classes(stylesheet="ported.css")
+    filepath = Rails.root.join("app/assets/stylesheets",stylesheet)
+    results = []
+    if File.file?(filepath)
+      content = File.read(filepath)
+      #css clases start with . and end with [,:. ]
+      content.scan(/\.[^\,^\:^\{^\.^\s]+/).uniq.sort.each do |i|
+        # filter out classes that picked up a .[number] as a class
+        results << i unless i[1].match(/\d/)
+      end
+    end
+    results
+  end
+
 end
