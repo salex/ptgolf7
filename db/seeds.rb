c903316c31c21cee55aff1a1951710ce2ac30bb5
Steve Alex <salex@mac.com>
2023-01-04 09:46:05 -0600

    fix gaggle shortcut login

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# The current conversion process
+
+# Get pt_golfer_production db from backups stored on macmini, unzip it
+
+# cd to pt_golfer 
+# bin/rails db:drop  --probably have to set env vairable
+# bin/rails db:create
+
+# psql -d pt_golfer_development < pt_golfer_production.sql
