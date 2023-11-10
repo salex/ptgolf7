b34f3e095b5bf85f44f3e20b302097424275b7c1
Steve Alex <salex@mac.com>
2023-11-09 18:03:40 -0600

    eager load false

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,8 +1,10 @@
+require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.cache_classes = true
+  # config.cache_classes = true
+  config.enable_reloading = false
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
