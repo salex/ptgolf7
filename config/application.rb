9a5e041479b476217fd70a17e79d242780f575ee
Steve Alex <salex@mac.com>
2023-11-11 09:11:19 -0600

    put yaml permit back

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Ptgolf
     #
     config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    # config.active_record.yaml_column_permitted_classes = [Symbol, Hash, Array, ActiveSupport::HashWithIndifferentAccess]
+    config.active_record.yaml_column_permitted_classes = [Symbol, Hash, Array, ActiveSupport::HashWithIndifferentAccess]
 
 
   end
