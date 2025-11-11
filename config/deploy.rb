bdce7372a4e387c7b748312edd2884008a4a85d6
Steve Alex <salex@mac.com>
2025-11-11 06:38:12 -0600

    try ssh to repo

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock "~> 3.19"
 
 set :application, "ptgolf"
 # set :repo_url, "developer@stevealex.us:/Users/developer/repo/#{fetch(:application)}.git"
-set :repo_url,  "git@github.com:salex/ptgolf7.git"
+set :repo_url,  "ssh://git@github.com:salex/ptgolf7.git"
 set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, File.read('.ruby-version').strip
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
