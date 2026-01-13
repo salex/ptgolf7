597e1d9c8f5ebbefdf9e731ab44929775a3c46ac
Steve Alex <salex@mac.com>
2026-01-13 12:19:24 -0600

    bundle update
    cap version changed

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,11 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.19"
+lock "~> 3.20"
 
 set :application, "ptgolf"
 # set :repo_url, "developer@stevealex.us:/Users/developer/repo/#{fetch(:application)}.git"
-set :repo_url,  "git@github.com:salex/ptgolf7.git"
+# set :repo_url,  "git@github.com:salex/ptgolf7.git"
+set :repo_url,  "https://github.com/salex/ptgolf7.git"
+
 set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, File.read('.ruby-version').strip
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
